cc00929495a5bc43f0898eee92fb2bcc25362787
munzii <munzii422@gmail.com>
2022-05-23 15:54:58 +0900

    2480

---
+++ Ch2/2480.py
@@ -0,0 +1,12 @@
+A, B, C = map(int, input().split())
+
+if A == B and B == C:
+    print(A * 1000 + 10000)
+elif A == B:
+    print(A * 100 + 1000)
+elif B == C:
+    print(B * 100 + 1000)
+elif A == C:
+    print(A * 100 + 1000)
+else:
+    print(100*max(A, B, C))
